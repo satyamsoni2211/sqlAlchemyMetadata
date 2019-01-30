d4f6909c25e44a53e88bbfd5877ef6adb17f5bea
satyam soni <satyamsoni@hotmail.co.uk>
2019-01-30 09:01:16 +0530

    enhanced wrapper

--- getMetadata.py
+++ getMetadata.py
@@ -4,32 +4,54 @@ from dicttoxml import dicttoxml
 
 
 class Meta:
+    '''
+        class to get metadata from any database
+        pass the connection string while creating object for the wrapper
+        say for sqlite i.e. sqlite:///satyam.db    
+        exposed methods:
+            getTables
+            getMetaForTable
+    '''
+
     def __init__(self, conn):
-        self.engine = create_engine(conn)
-        self.metadata = MetaData()
-        self.metadata.reflect(bind=self.engine)
+        self.__engine = create_engine(conn)
+        self.__metadata = MetaData()
+        self.__metadata.reflect(bind=self.__engine)
 
     def getTables(self):
         ''' function for getting all the tables in the schema
             this list all the available functions'''
-        return self.metadata.tables.keys()
+        return self.__metadata.tables.keys()
 
     def getMetaForTable(self, table):
         ''' this is for getting metadata for the given table
             if the table is not found this raises an Exception'''
-        if table in self.metadata.tables.keys():
+        if table in self.__metadata.tables.keys():
             # for getting the table metadata
             tableMeta = dict(
                 [(table, dict(
-                    [(i, self.filterColMeta(j))
-                     for i, j in self.metadata.tables[table].columns.items()]
+                    [(i, self.__filterColMeta(j))
+                     for i, j in self.__metadata.tables[table].columns.items()]
                 ))])
             return tableMeta
         else:
             raise Exception('Table Not Found')
 
-    def filterColMeta(self, data):
-        ''' for converting column object to keys 
+    def __parseCols(self, columMeta):
+        '''
+            formatting columns with class objects
+        '''
+        # converting type to str representation
+        columMeta['type'] = columMeta.pop('type').__str__()
+        # convert the Foriegn key object to dict and string
+        columMeta['foreign_keys'] = map(
+            str, list(columMeta.pop('foreign_keys')))
+        # set to list conversion
+        columMeta['constraints'] = list(columMeta.pop('constraints'))
+        return columMeta
+
+    def __filterColMeta(self, data):
+        ''' for converting column object to keys
             and parsing the data and objects to JSON serializable
             format so that it can be dumped as JSON'''
         allowedKeys = ['comment',
@@ -45,19 +67,14 @@ class Meta:
                        'name',
                        'onupdate',
                        'foreign_keys',
-                       #    'server_default',
                        '_creation_order',
                        'doc',
                        'unique',
                        'constraints',
                        'type']
-        columMeta = dict([(i, j.__str__()) if i == 'type' else (i, j)
-                          for i, j in data.__dict__.items() if i in allowedKeys])
-        # convert the Foriegn key object to dict and string
-        columMeta['foreign_keys'] = map(
-            str, list(columMeta.pop('foreign_keys')))
-        # set to list conversion
-        columMeta['constraints'] = list(columMeta.pop('constraints'))
+        columMeta = self.__parseCols(dict([(i, j)
+                                           for i, j in data.__dict__.items() if i in allowedKeys]))
+
         return columMeta
 
 
